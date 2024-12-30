974083b28de0a718b7cb61de0d4b997feb61ed83
nandini-2108 <alapatinandini93@gmail.com>
2024-12-30 15:59:21 +0530

    Add files via upload

---
+++ arith.js
@@ -0,0 +1,6 @@
+const name=require('./server1');
+console.log(name.addition(21,4));
+console.log(name.subtraction(21,4));
+console.log(name.multiplication(21,4));
+console.log(name.division(21,4));
+console.log(name.remainder(21,4));
